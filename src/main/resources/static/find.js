9ef01be9b0699180703c96b3f18d822d370500ff
yboodhan <yboodhan@gmail.com>
2020-02-20 14:17:41 -0800

    Video getting is set up and working, API enabled

--- src/main/resources/static/find.js
+++ src/main/resources/static/find.js
@@ -9,9 +9,33 @@ $(function() {
         // }
         function loadClient() {
             gapi.client.setApiKey(API_KEY);
+
             return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-                .then(function() { console.log("GAPI client loaded for API"); },
-                    function(err) { console.error("Error loading GAPI client for API", err); });
-        }
+            .then(
+                function() {
+                    console.log("GAPI client loaded for API");
+                    execute();
+                })
+            .catch(
+                function(err) {
+                    console.error("Error loading GAPI client for API", err);
+                }
+            )}
     )
+
+    function execute() {
+        return gapi.client.youtube.search.list({
+            q: 'dogs',
+            part: 'snippet',
+            maxResults: 10
+        })
+        .then(function(response) {
+            // Handle the results here (response.result has the parsed body).
+            console.log("Response", response);
+        })
+        .catch(
+            function(err) {
+                console.error("Execute error", err);
+            }
+        )}
 });
