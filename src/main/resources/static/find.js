dcb94d05748ae55c863da5f1998b2a05c8590468
yboodhan <yboodhan@gmail.com>
2020-02-20 15:16:10 -0800

    Removed autocomplete and render results from youtube

--- src/main/resources/static/find.js
+++ src/main/resources/static/find.js
@@ -1,41 +1,51 @@
-$(function() {
+var API_KEY = GOOGLE_KEY
 
-    var API_KEY = GOOGLE_KEY
+$('#search').click(
+    function loadClient() {
+        $('#results').append('<pre>' + 'Searching now...' + '</pre>');
+        gapi.client.setApiKey(API_KEY);
 
-    $('#searcher').click(
-        // function searchByKeyword() {
-        //     var results = YouTube.Search.list('id,snippet', {q: 'dogs', maxResults: 25});
-        //     console.log(results)
-        // }
-        function loadClient() {
-            gapi.client.setApiKey(API_KEY);
-
-            return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-            .then(
-                function() {
-                    console.log("GAPI client loaded for API");
-                    execute();
-                })
-            .catch(
-                function(err) {
-                    console.error("Error loading GAPI client for API", err);
-                }
-            )}
-    )
-
-    function execute() {
-        return gapi.client.youtube.search.list({
-            q: 'dogs',
-            part: 'snippet',
-            maxResults: 10
-        })
-        .then(function(response) {
-            // Handle the results here (response.result has the parsed body).
-            console.log("Response", response);
-        })
+        return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+        .then(
+            function() {
+                console.log("GAPI client loaded for API");
+                execute();
+            })
         .catch(
             function(err) {
-                console.error("Execute error", err);
+                console.error("Error loading GAPI client for API", err);
             }
         )}
-});
+)
+
+function execute() {
+    return gapi.client.youtube.search.list({
+        q: $('#query').val(),
+        part: 'snippet',
+        maxResults: 20
+    })
+    .then(function(response) {
+        // Handle the results here (response.result has the parsed body).
+        $('#results').empty();
+        var allResultVideos = response.result.items;
+
+        // The call was successful but no videos matched query
+        if (allResultVideos.length <= 0) {
+            console.log('No search results')
+            return
+        }
+
+        // There are actual results!
+        $.each(allResultVideos, function(index, video){
+            var vidTitle = video.snippet.title;
+            var vidThumburl =  video.snippet.thumbnails.default.url;
+            var vidThumbimg = '<pre><img id="thumb" src="'+vidThumburl+'" alt="No  Image  Available." style="width:204px;height:128px"></pre>';
+
+            $('#results').append('<pre>' + vidTitle + vidThumbimg +   '</pre>');
+        })
+    })
+    .catch(
+        function(err) {
+            console.error("Execute error", err);
+        }
+    )};
